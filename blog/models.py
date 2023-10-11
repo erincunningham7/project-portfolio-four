27c75a3c05c90974ab1fe5521e8940a3ffaeb982
erincunningham7 <>
2023-10-11 11:24:01 +0000

    format document and remove import error

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from Django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
 
 class BlogPost(models.Model):
     DRAFT = 'draft'
@@ -15,14 +16,13 @@ class BlogPost(models.Model):
     date_created = models.DateTimeField(auto_now=True)
     content = models.TextField()
     image = CloudinaryField('image', default='placeholder')
-    status = models.CharField(max_length=6, choices=STATUS_CHOICES, default=DRAFT)
+    status = models.CharField(
+        max_length=6, choices=STATUS_CHOICES, default=DRAFT)
     likes = models.ManyToManyField(User, related_name='likes', blank=True)
-    
 
     def __str__(self):
         return self.title
 
-    
     class Meta:
         ordering = ["-created_on"]
 
