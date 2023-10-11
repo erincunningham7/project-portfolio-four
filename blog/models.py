984d9becde0961aed28bb52d9d2cb71bb8320692
erincunningham7 <>
2023-10-11 11:32:40 +0000

    add comments and change likes related name in models.py

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+# import user model
 from django.contrib.auth.models import User
+# import cloudinary field for images
 from cloudinary.models import CloudinaryField
 
-
+# create blog post class based model inheriting from standard model
 class BlogPost(models.Model):
+
+    # define a BlogPost model with a status field that can have one of two 
+    # choices: 'Draft' or 'Posted'
     DRAFT = 'draft'
     POSTED = 'posted'
 
@@ -18,13 +23,16 @@ class BlogPost(models.Model):
     image = CloudinaryField('image', default='placeholder')
     status = models.CharField(
         max_length=6, choices=STATUS_CHOICES, default=DRAFT)
-    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
 
+    # return string representation of title
     def __str__(self):
         return self.title
 
+    # apply descending ordering to posts
     class Meta:
         ordering = ["-created_on"]
 
+    # helper method to return num of likes on a post
     def number_of_likes(self):
         return self.likes.count()
