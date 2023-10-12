b1b2524e700af68363605816232b26e624bbdef7
erincunningham7 <>
2023-10-12 16:46:35 +0000

    import quill in models.py

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # import cloudinary field for images
 from cloudinary.models import CloudinaryField
+from django_quill.fields import QuillField
 
 # create blog post class based model inheriting from standard model
 
