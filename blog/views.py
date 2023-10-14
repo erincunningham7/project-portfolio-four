b5d231741c54ef7aca27d200f0138aaa7594bfa4
erincunningham7 <>
2023-10-14 19:47:27 +0000

    create custom queryset to filter posted blogpost in views.py

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,12 @@ from .models import BlogPost
 
 class BlogListView(generic.ListView):
     model = BlogPost
-    queryset = BlogPost.objects.filter(
-        status=POSTED).order_by('-date_created')
+    # queryset = BlogPost.objects.filter(
+    #    status=POSTED).order_by('-date_created')
     paginate_by = 3
     template_name = 'index.html'
+
+    def get_queryset(self):
+        # Filter only 'Posted' blog posts
+        queryset = BlogPost.objects.filter(status='Posted')
+        return queryset
